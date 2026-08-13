7c80cbd53d53f753878080bca0bb1ad64ff5789e
Aashish Mahto <149124860+Aashish774-star@users.noreply.github.com>
2026-08-13 23:56:01 +0530

    Comment out Solution class in next greater element II
    Commented out the Solution class and its method for finding the next greater elements.

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -39,3 +39,38 @@ public:
         return res; 
     } 
 };
+
+
+/*
+class Solution { 
+public: 
+    vector<int> nextGreaterElements(vector<int>& nums) { 
+        int n = nums.size(); 
+        vector<int> res(n);
+        stack<int> st;
+        if(n == 1)
+        return res; 
+ 
+        st.push(nums[n - 1]); 
+ 
+        for(int i = n - 2; i >= 0; i--) 
+        { 
+            st.push(nums[i]);
+            for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i]) 
+            { 
+                st.pop(); 
+            } 
+ 
+            if(st.empty()) 
+            res[i] = -1; 
+            else 
+            res[i] = st.top(); 
+ 
+            st.push(nums[i]); 
+        }
+        }
+        return res;
+    } 
+};
+*/
