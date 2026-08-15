2bff1f38ae0491bf476b7125b1be037210b46ccc
Aashish Mahto <149124860+Aashish774-star@users.noreply.github.com>
2026-08-15 16:36:10 +0530

    Time: 0 ms (100%), Space: 11.3 MB (40.38%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,55 +1,66 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        // Empty list
         if(head == NULL)
         {
             return NULL;
         }
+
+        // Agar sirf ek hi node reverse karna hai
         if(left == right)
-        return head;
+            return head;
 
         ListNode* t = head;
         ListNode* before = NULL;
         int pos = 1;
 
+        // Left position tak pahuchna
         while(t != NULL)
         {
             if(pos < left)
             {
                 before = t;
-                t = t -> next;
+                t = t->next;
                 pos++;
                 continue;
             }
 
+            // Reverse karne ke liye pointers
             ListNode* curr = t;
             ListNode* prev = NULL;
+
+            // Kitne nodes reverse karne hain
             int times = right - left + 1;
+
             while(times--)
             {
-                ListNode* nex = curr -> next;
-                curr -> next = prev;
+                ListNode* nex = curr->next;
+
+                // Current node ko reverse direction me point karna
+                curr->next = prev;
+
                 prev = curr;
                 curr = nex;
             }
-            t -> next = curr;
-            if(before){
-            before -> next = prev;
-            return head;
+
+            // Reversed part ke last node ko remaining list se connect
+            t->next = curr;
+
+            // Agar left = 1 hai, to prev new head hoga
+            if(before)
+            {
+                // Reversed part ko previous part se connect
+                before->next = prev;
+                return head;
             }
             else
-            return prev;
+            {
+                // left = 1, isliye prev new head banega
+                return prev;
+            }
         }
+
         return head;
     }
 };
